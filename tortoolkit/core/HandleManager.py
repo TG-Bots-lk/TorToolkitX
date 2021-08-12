ec49124e2e3b230b4187b9d19fa79908445b97c8
Irupc <68991705+Irupc@users.noreply.github.com>
2021-08-12 09:16:02 +0530

    Update HandleManager.py

--- tortoolkit/core/HandleManager.py
+++ tortoolkit/core/HandleManager.py
@@ -643,7 +643,7 @@ async def set_password_zip(message):
 
 
 async def start_handler(event):
-    msg = "Hello This is TorToolkitX running on heroku an instance of <a href='https://github.com/XcodersHub/TorToolkitX'>This Repo</a>. Try the repo for yourself and dont forget to put a STAR and fork."
+    msg = "😋 Hi I'm Telegram File Uploader Bot Created by <a href='https://t.me/tgbotslk'>TG 𝔹𝕠𝕥𝕤 LK</a> & I'm Working only in <b>@FlashMirrorLk</b> Group"
     await event.reply(msg, parse_mode="html")
 
 
